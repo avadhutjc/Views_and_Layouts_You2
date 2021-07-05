9bcf14c45387d66f8711fdbc2fd012c8201f53c7
Avadhutjc <Avadhutjc@gmail.com>
2021-07-05 23:51:52 +0530

    first committed project

---
+++ app/src/main/java/com/masai/views_and_layouts_you2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.masai.views_and_layouts_you2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
